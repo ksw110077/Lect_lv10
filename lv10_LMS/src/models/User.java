831560b3fa2102c6ba1bf4e9caa988aaa7a88a75
Administrator <Administrator@121.162.238.156>
2021-10-26 16:24:25 +0900

    중간 커밋

--- lv10_LMS/src/models/User.java
+++ lv10_LMS/src/models/User.java
@@ -1,10 +1,20 @@
 package models;
 
+import java.util.ArrayList;
+
 public abstract class User {
 	private String name;
 	private String id;
 	private String pw;
+	private int majorCode;
+	private int subCode;
+	private int userCode;
+	private ArrayList<Sub> sub = new ArrayList<Sub>();
 	
+	public User(String n, String i, String p, int mc, int sc, int uc) {
+		this.name = n;		this.id = i;		this.pw = p;
+		this.majorCode = mc;		this.subCode = sc; this.userCode = uc;
+	}
 	
 	abstract String printAll(); // 출력
 }

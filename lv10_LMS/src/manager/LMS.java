ff6e04f0b2a7e2650b06696672d5f2ebcde0092d
Administrator <Administrator@121.162.238.156>
2021-10-26 21:18:26 +0900

    User Manager

--- lv10_LMS/src/manager/LMS.java
+++ lv10_LMS/src/manager/LMS.java
@@ -21,15 +21,19 @@ public class LMS {
 		return sel;
 	}
 	
+	private void printLMS() {
+		System.out.println("======= LMS =======");
+	}
+	
 	private void printLMSMenu() {
 		System.out.println("[1.][학생등록]\n[2.][학생탈퇴]\n"
 				+ "[3.][수강신청]\n[4.][성적관리]\n[0.][시스템종료]");
 	}
 	
-	private void selLMSMenu() {
+	private boolean selLMSMenu() {
 		int sel = inputSystem();
 		if(sel == 1) {
-			
+			UM.getinstance().printUMMenu();
 		}
 		else if (sel == 2) {
 			
@@ -41,10 +45,18 @@ public class LMS {
 			
 		}
 		else if (sel == 0) {
-			
+			System.out.println("시스템을 종료합니다.");
+			return false;
 		}
+		return true;
 	}
 	
 	public void run() {
+		boolean isRun = true;
+		while(isRun) {
+			printLMS();
+			printLMSMenu();
+			isRun = selLMSMenu();
+		}
 	}
 }
--- lv10_LMS/src/manager/UM.java
+++ lv10_LMS/src/manager/UM.java
@@ -8,4 +8,7 @@ public class UM {
 		return instance;
 	}
 	
+	public void printUMMenu() {
+	}
+	
 }

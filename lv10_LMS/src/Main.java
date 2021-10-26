2d65dda797ec1c1b1f4cc8566fc11c1fd86e843b
Administrator <Administrator@121.162.238.156>
2021-10-26 16:14:45 +0900

    start

---
+++ lv10_LMS/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		파일 처리 시작하면 로드
+		
+		1.학생 관리
+		
+		ㄴ 가입
+		User
+		ㄴ ID
+		ㄴ pw
+		ㄴ 학과 코드
+		ㄴ 과목 코드
+		Student
+		Professor
+		
+		
+		ㄴ 탈퇴
+		
+		
+
+		2. 수강 신청
+		ㄴ 학과 확인 
+		ㄴ 과목 조회
+		3. 성적 관리
+		ㄴ 성적확인
+		ㄴ 성적 수정
+		
+		종료 할때 저장 처리
+		*/
+	}
+
+}
---
+++ lv10_LMS/src/models/User.java
@@ -0,0 +1,10 @@
+package models;
+
+public abstract class User {
+	private String name;
+	private String id;
+	private String pw;
+	
+	
+	abstract String printAll(); // 출력
+}

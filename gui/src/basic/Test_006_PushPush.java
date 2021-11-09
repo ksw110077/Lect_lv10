d4fe08303c9003083e52fcbccc16ddbce4761651
Administrator <Administrator@121.162.238.82>
2021-11-09 20:48:51 +0900

    push

--- gui/src/basic/Test_006_PushPush.java
+++ gui/src/basic/Test_006_PushPush.java
@@ -1,18 +1,31 @@
 package basic;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Random;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 
 class PushNemo{
-	private int x, y, w, h, owner;
+	private int x, y, w, h;
 	private Color c;
-	public PushNemo() {
+	public PushNemo(int x, int y, int w, int h) {
 		this.x = x;
 		this.y = y;
 		this.w = w;
 		this.h = h;
-		this.c = c;
+		this.c = Color.black;
 	}
 	
 	public int getX() {
@@ -30,20 +43,8 @@ class PushNemo{
 	public int getW() {
 		return w;
 	}
-	public void setW(int w) {
-		this.w = w;
-	}
 	public int getH() {
 		return h;
-	}
-	public void setH(int h) {
-		this.h = h;
-	}
-	public int getOwner() {
-		return owner;
-	}
-	public void setOwner(int owner) {
-		this.owner = owner;
 	}
 	public Color getC() {
 		return c;
@@ -53,16 +54,345 @@ class PushNemo{
 	}
 }
 
-class PushFrame extends JFrame{
-	public PushFrame() {
+class PushPanel extends JPanel implements MouseListener, Runnable{
+	private final int UP = 0;
+	private final int LEFT = 1;
+	private final int DOWN = 2;
+	private final int RIGHT = 3;
+	
+	private final int SIZE = PushFrame.SIZE;
+	
+	
+	private PushNemo move;
+	private PushNemo stop;
+	private JButton[] arrow = new JButton[4];
+	private boolean [] arrowChk = new boolean [4];
+	private boolean chk = false;
+	
+	public PushPanel() {
+		setLayout(null);
+		setBounds(0,0,this.SIZE, this.SIZE);
+		setBackground(Color.white);
+		setArrow();
+		setNemo();
 	}
 	
+	private void setNemo() {
+		Random rn = new Random();
+		int rX = rn.nextInt(this.SIZE - 100);
+		int rY = rn.nextInt(this.SIZE - 100);
+		
+		this.stop = new PushNemo(rX, rY, 100, 100);
+		
+		while(true) {
+			rX = rn.nextInt(this.SIZE - 100);
+			rY = rn.nextInt(this.SIZE - 100);
+			
+			boolean check = false;
+			// 검증
+			if(rX < this.stop.getX() || rX >= this.stop.getX() + this.stop.getW() 
+			&&rY < this.stop.getY() || rY >= this.stop.getY() + this.stop.getH()) {
+				check = true;
+			}
+			
+			if(check) {
+				this.move = new PushNemo(rX, rY, 100, 100);
+				break;
+			}
+		}
+	}
+	
+	private void setArrow() {
+		int x = PushFrame.SIZE - 50 - 50 - 10 - 50;
+		int y = PushFrame.SIZE - 50 - 50 - 10 - 50;
+		for(int i =0; i < this.arrow.length; i++) {
+			this.arrow[i] = new JButton();
+			if(i == 0) {
+				this.arrow[i].setText("↑");
+				this.arrow[i].setBounds(x, y, 50, 50);
+				x += - 10 - 50;
+				y += 50 + 10;
+			}
+			else {
+				if(i == 1) {
+					this.arrow[i].setText("←");
+					this.arrow[i].setBounds(x, y, 50, 50);
+					x += 50 + 10;
+				}
+				else if (i == 2) {
+					this.arrow[i].setText("↓");
+					this.arrow[i].setBounds(x, y, 50, 50);
+					x += 50 + 10;
+				}
+				else if (i == 3) {
+					this.arrow[i].setText("→");
+					this.arrow[i].setBounds(x, y, 50, 50);
+				}
+			}
+			this.arrow[i].setFont(new Font("NanumGothicBold",Font.PLAIN, 17));
+			this.arrow[i].setBackground(new Color(249, 243, 223));
+			this.arrow[i].addMouseListener(this);
+			add(this.arrow[i]);
+		}
+	}
+
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		if(this.move != null && this.stop != null) {
+			g.setColor(Color.black);
+			g.drawRect(this.move.getX(), this.move.getY(), this.move.getW(), this.move.getH());
+			
+			g.setColor(Color.blue);
+			g.drawRect(this.stop.getX(), this.stop.getY(), this.stop.getW(), this.stop.getH());
+		}
+		repaint();
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if(e.getSource() instanceof JButton) {
+			JButton target = (JButton) e.getSource();
+			if(target == this.arrow[this.UP]) {
+				this.arrowChk[this.UP] = true;
+			}
+			else if(target == this.arrow[this.LEFT]) {
+				this.arrowChk[this.LEFT] = true;
+			}
+			else if(target == this.arrow[this.DOWN]) {
+				this.arrowChk[this.DOWN] = true;
+			}
+			else if(target == this.arrow[this.RIGHT]) {
+				this.arrowChk[this.RIGHT] = true;
+			}
+		}
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		if(e.getSource() instanceof JButton) {
+			JButton target = (JButton) e.getSource();
+			if(target == this.arrow[this.UP]) {
+				this.arrowChk[this.UP] = false;
+			}
+			else if(target == this.arrow[this.LEFT]) {
+				this.arrowChk[this.LEFT] = false;
+			}
+			else if(target == this.arrow[this.DOWN]) {
+				this.arrowChk[this.DOWN] = false;
+			}
+			else if(target == this.arrow[this.RIGHT]) {
+				this.arrowChk[this.RIGHT] = false;
+			}
+		}
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private boolean chkOverlapLeft() {
+		boolean chk = false;
+		
+		// stop 오른쪽과 move 왼쪽 닿을때
+		// stop.getX() + stop.getW() == move.getX();
+		
+		int mX = this.move.getX();
+		int mY = this.move.getY();
+		int mW = this.move.getW();
+		int mH = this.move.getH();
+		
+		int sX = this.stop.getX();
+		int sY = this.stop.getY();
+		int sW = this.stop.getW();
+		int sH = this.stop.getH();
+		
+		if(sX + sW == mX && ((mY >= sY && mY < sY + sH) || (mY + mH > sY && mY + mH <= sY + sH))) {
+			chk = true;
+		}
+		return chk;
+	}
+	private boolean chkOverlapRight() {
+		boolean chk = false;
+		// stop 왼쪽과 move 오른쪽 닿을때
+		// stop.getX() == move.getX() + move.getW();
+		
+		int mX = this.move.getX();
+		int mY = this.move.getY();
+		int mW = this.move.getW();
+		int mH = this.move.getH();
+		
+		int sX = this.stop.getX();
+		int sY = this.stop.getY();
+		int sW = this.stop.getW();
+		int sH = this.stop.getH();
+		
+		if(sX == mX + mW && ((mY >= sY && mY < sY + sH) || (mY + mH > sY && mY + mH <= sY + sH))) {
+			chk = true;
+		}
+		
+		return chk;
+	}
+	
+	
+	private boolean chkOverlapDown() {
+		boolean chk = false;
+		// stop 윗쪽과 move 아래쪽 닿을때
+		// stop.getY() == move.getY() + move.getH() ;
+		
+		int mX = this.move.getX();
+		int mY = this.move.getY();
+		int mW = this.move.getW();
+		int mH = this.move.getH();
+		
+		int sX = this.stop.getX();
+		int sY = this.stop.getY();
+		int sW = this.stop.getW();
+		int sH = this.stop.getH();
+		
+		if(sY == mY + mH && ((mX >= sX && mX < sX + sW) || (mX + mW > sX && mX + mW <= sX + sW))) {
+			chk = true;
+		}
+		return chk;
+	}
+	
+	
+	private boolean chkOverlapUP() {
+		boolean chk = false;
+		// stop 아래쪽과  move 윗쪽 닿을때
+		// stop.getY() + stop.getH() == move.getY();
+		int mX = this.move.getX();
+		int mY = this.move.getY();
+		int mW = this.move.getW();
+		int mH = this.move.getH();
+		
+		int sX = this.stop.getX();
+		int sY = this.stop.getY();
+		int sW = this.stop.getW();
+		int sH = this.stop.getH();
+		
+		if(sY + sH == mY && ((mX >= sX && mX < sX + sW) || (mX + mW > sX && mX + mW <= sX + sW))) {
+			chk = true;
+		}
+		return chk;
+	}
+	
+	private void up() {
+		if(chkOverlapUP()) { // 같이 이동
+			if(this.stop.getY() > 0) {
+				this.move.setY(this.move.getY() - 1);
+				this.stop.setY(this.stop.getY() - 1);
+			}
+		}
+		else { // move만 이동
+			if(this.move.getY() > 0) {
+				this.move.setY(this.move.getY() - 1);
+			}
+		}
+	}
+	
+	private void left() {
+		if(chkOverlapLeft()) { // 같이 이동
+			if(this.stop.getX() > 0) {
+				this.move.setX(this.move.getX() - 1);
+				this.stop.setX(this.stop.getX() - 1);
+			}
+		}
+		else { // move만 이동
+			if(this.move.getX() > 0) {
+				this.move.setX(this.move.getX() - 1);
+			}
+		}
+	}
+	
+	private void down() {
+		if(chkOverlapDown()) { // 같이 이동
+			if(this.stop.getY() + this.stop.getH() < this.SIZE) {
+				this.move.setY(this.move.getY() + 1);
+				this.stop.setY(this.stop.getY() + 1);
+			}
+		}
+		else { // move만 이동
+			if(this.move.getY() + this.move.getH() < this.SIZE) {
+				this.move.setY(this.move.getY() + 1);
+			}
+		}
+	}
+	
+	private void right() {
+		if(chkOverlapRight()) { // 같이 이동
+			if(this.stop.getX() + this.stop.getH() < this.SIZE) {
+				this.move.setX(this.move.getX() + 1);
+				this.stop.setX(this.stop.getX() + 1);
+			}
+		}
+		else { // move만 이동
+			if(this.move.getX() + this.move.getH() < this.SIZE) {
+				this.move.setX(this.move.getX() + 1);
+			}
+		}
+	}
+	
+	@Override
+	public void run() {
+		while(true) {
+			if(this.arrowChk[this.UP]) {
+				up();
+			}
+			else if (this.arrowChk[this.LEFT]) {
+				left();
+			}
+			else if (this.arrowChk[this.DOWN]) {
+				down();
+			}
+			else if (this.arrowChk[this.RIGHT]) {
+				right();
+			}
+			
+			try {
+				Thread.sleep(10); // 0.01 초
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+	}
+
+}
+
+class PushFrame extends JFrame{
+	private PushPanel panel = new PushPanel();
+	private Dimension dm = Toolkit.getDefaultToolkit().getScreenSize();
+	private final int W = dm.width;
+	private final int H = dm.height;
+	public static final int SIZE = 800;
+	
+	public PushFrame() {
+		setLayout(null);
+		setTitle("Push Push");
+		setBounds(W / 2 - this.SIZE / 2, H / 2 - this.SIZE / 2, this.SIZE, this.SIZE);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		add(this.panel);
+		setVisible(true);
+		revalidate();
+		this.panel.run();
+	}
 	
 }
 
 public class Test_006_PushPush {
 
 	public static void main(String[] args) {
+		PushFrame game = new PushFrame();
 	}
 
 }
---
+++ gui/src/basic/Test_006_PushPush_정답.java
@@ -0,0 +1,248 @@
+package basic;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+class PushNemo02{
+	private int x, y, w, h;
+	private Color c;
+	public PushNemo02(int x, int y, int w, int h) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.c = Color.black;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getW() {
+		return w;
+	}
+	public int getH() {
+		return h;
+	}
+	public Color getC() {
+		return c;
+	}
+	public void setC(Color c) {
+		this.c = c;
+	}
+}
+
+class PushPanel02 extends JPanel implements ActionListener, MouseListener{
+	
+	private final int LEFT = 0;
+	private final int DOWN = 1;
+	private final int RIGHT = 2;
+	private final int UP = 3;
+	
+	private final int SIZE = 700;
+	
+	private JButton [] btn = new JButton[4];
+	
+	private PushNemo02 nemo1 = null;
+	private PushNemo02 nemo2 = null;
+	
+	private int dir; // 0Left 1Down 2Right 3Up
+	private boolean isMoving;
+	
+	public PushPanel02() {
+		setLayout(null);
+		setBounds(0,0,this.SIZE, this.SIZE);
+		setBackground(Color.white);
+		this.dir = 5;
+		setBtn();
+		setNemo();
+	}
+
+	private void setNemo() {
+		Random rn = new Random();
+		int rX = rn.nextInt(this.SIZE - 100);
+		int rY = rn.nextInt(this.SIZE - 100);
+		
+		this.nemo1 = new PushNemo02(rX, rY, 100, 100);
+		
+		// nemo2는 nemo1과 겹쳐지지 않도록 중복 확인
+		
+		while(true) {
+			rX = rn.nextInt(this.SIZE - 100);
+			rY = rn.nextInt(this.SIZE - 100);
+			
+			// 검증
+			boolean check = false;
+			
+			if(!check) {
+				this.nemo2 = new PushNemo02(rX, rY, 100, 100);
+				break;
+			}
+		}
+	}
+	
+	private void setBtn() {
+		String []  text = {"←","↓","→","↑"};
+		int x = 500;
+		int y = 550;
+		for(int i =0; i < 4; i++) {
+			JButton bt = new JButton();
+			bt.setBounds(x,y,50,50);
+			bt.setText(text[i]);
+//			bt.addActionListener(this); // this : bt
+			bt.addMouseListener(this);
+			add(bt);
+			
+			this.btn[i] = bt;
+
+			x += 50;
+			if(i == this.btn.length -2) {
+				x -= 100;
+				y -= 50;
+			}
+			
+		}
+	}
+	
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		g.setColor(Color.black);
+		g.drawRect(this.nemo1.getX(), this.nemo1.getY(), this.nemo1.getW(), this.nemo1.getH());
+
+		// draw Rect
+//		if(this.nemo1 != null && this.nemo2 != null) {
+//			g.setColor(Color.black);
+//			g.drawRect(this.nemo1.getX(), this.nemo1.getY(), this.nemo1.getW(), this.nemo1.getH());
+//
+//			g.setColor(Color.blue);
+//			g.drawRect(this.nemo2.getX(), this.nemo2.getY(), this.nemo2.getW(), this.nemo2.getH());
+//		}
+		
+		if(isMoving) {
+			update();
+		}
+		
+		repaint();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		System.out.println("press");
+		this.isMoving = true;
+		
+		// check Button
+		if(e.getSource() instanceof JButton) {
+			JButton target = (JButton) e.getSource();
+			
+			if(target == this.btn[this.LEFT]) {
+				this.dir = this.LEFT;
+			}
+			else if(target == this.btn[this.DOWN]) {
+				this.dir = this.DOWN;
+			}
+			else if(target == this.btn[this.RIGHT]) {
+				this.dir = this.RIGHT;
+			}
+			else if(target == this.btn[this.UP]) {
+				this.dir = this.UP;
+			}
+		}
+		
+	}
+
+	private void update() {
+		if(this.dir == this.LEFT) {
+			this.nemo1.setX(this.nemo1.getX() -1);
+		}
+		if(this.dir == this.DOWN) {
+			this.nemo1.setY(this.nemo1.getY() +1);
+		}
+		if(this.dir == this.RIGHT) {
+			this.nemo1.setX(this.nemo1.getX() +1);
+		}
+		if(this.dir == this.UP) {
+			this.nemo1.setY(this.nemo1.getY() -1);
+		}
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		this.isMoving = false;
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
+
+class PushFrame02 extends JFrame{
+	public PushFrame02() {
+		setLayout(null);
+		setTitle("Push Push");
+		setBounds(50,50,700,700);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
+		add(new PushPanel02());
+		
+		
+		setVisible(true);
+		revalidate();
+	}
+	
+}
+
+public class Test_006_PushPush_정답 {
+
+	public static void main(String[] args) {
+		PushFrame02 game = new PushFrame02();
+	}
+
+}

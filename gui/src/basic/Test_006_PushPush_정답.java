f0b4f1ea1239225a82bc85d50403b46bb4472c96
Administrator <Administrator@121.162.238.82>
2021-11-10 16:39:07 +0900

    선생님 풀이

--- gui/src/basic/Test_006_PushPush_정답.java
+++ gui/src/basic/Test_006_PushPush_정답.java
@@ -90,17 +90,17 @@ class PushPanel02 extends JPanel implements ActionListener, MouseListener{
 		// nemo2는 nemo1과 겹쳐지지 않도록 중복 확인
 		
 		while(true) {
-			rX = rn.nextInt(this.SIZE - 100);
-			rY = rn.nextInt(this.SIZE - 100);
+			rX = rn.nextInt(this.SIZE - 100 - 100);
+			rY = rn.nextInt(this.SIZE - 100 - 100);
 			
 			// 검증
-			boolean check = false;
 			
-			if(!check) {
-				this.nemo2 = new PushNemo02(rX, rY, 100, 100);
+			if(rX < this.nemo1.getX() || rX > this.nemo1.getX() + this.nemo1.getW() 
+			|| rY < this.nemo1.getY() || rY > this.nemo1.getY() + this.nemo1.getH()) {
 				break;
 			}
 		}
+		this.nemo2 = new PushNemo02(rX, rY, 100, 100);
 	}
 	
 	private void setBtn() {
@@ -131,17 +131,15 @@ class PushPanel02 extends JPanel implements ActionListener, MouseListener{
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		g.setColor(Color.black);
-		g.drawRect(this.nemo1.getX(), this.nemo1.getY(), this.nemo1.getW(), this.nemo1.getH());
 
 		// draw Rect
-//		if(this.nemo1 != null && this.nemo2 != null) {
-//			g.setColor(Color.black);
-//			g.drawRect(this.nemo1.getX(), this.nemo1.getY(), this.nemo1.getW(), this.nemo1.getH());
-//
-//			g.setColor(Color.blue);
-//			g.drawRect(this.nemo2.getX(), this.nemo2.getY(), this.nemo2.getW(), this.nemo2.getH());
-//		}
+		if(this.nemo1 != null && this.nemo2 != null) {
+			g.setColor(Color.black);
+			g.drawRect(this.nemo1.getX(), this.nemo1.getY(), this.nemo1.getW(), this.nemo1.getH());
+
+			g.setColor(Color.blue);
+			g.drawRect(this.nemo2.getX(), this.nemo2.getY(), this.nemo2.getW(), this.nemo2.getH());
+		}
 		
 		if(isMoving) {
 			update();
@@ -189,16 +187,58 @@ class PushPanel02 extends JPanel implements ActionListener, MouseListener{
 
 	private void update() {
 		if(this.dir == this.LEFT) {
-			this.nemo1.setX(this.nemo1.getX() -1);
+			if(this.nemo1.getX() > 0 ) {
+				this.nemo1.setX(this.nemo1.getX() -1);
+			}
 		}
 		if(this.dir == this.DOWN) {
-			this.nemo1.setY(this.nemo1.getY() +1);
+			if(this.nemo1.getY() < this.SIZE - this.nemo1.getH()) {
+				this.nemo1.setY(this.nemo1.getY() +1);
+			}
 		}
 		if(this.dir == this.RIGHT) {
-			this.nemo1.setX(this.nemo1.getX() +1);
+			if(this.nemo1.getX() < this.SIZE - this.nemo1.getW() ) {
+				this.nemo1.setX(this.nemo1.getX() +1);
+			}
 		}
 		if(this.dir == this.UP) {
-			this.nemo1.setY(this.nemo1.getY() -1);
+			if(this.nemo1.getY() > 0) {
+				this.nemo1.setY(this.nemo1.getY() -1);
+			}
+		}
+		checkSecond();
+//		try {
+//			Thread.sleep(5);
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//		}
+	}
+
+	private void checkSecond() {
+		if (this.dir == this.LEFT) {
+			if (this.nemo2.getX() + this.nemo2.getW() == this.nemo1.getX()
+					&& this.nemo2.getY() > this.nemo1.getY() - this.nemo1.getH()
+					&& this.nemo2.getY() < this.nemo1.getY() + this.nemo1.getH()) {
+				this.nemo2.setX(this.nemo2.getX() - 1);
+			}
+		} else if (this.dir == this.DOWN) {
+			if (this.nemo2.getY() == this.nemo1.getY() + this.nemo1.getH()
+					&& this.nemo2.getX() > this.nemo1.getX() - this.nemo1.getW()
+					&& this.nemo2.getX() < this.nemo1.getX() + this.nemo1.getW()) {
+				this.nemo2.setY(this.nemo2.getY() + 1);
+			}
+		} else if (this.dir == this.RIGHT) {
+			if (this.nemo2.getX() == this.nemo1.getX() + this.nemo1.getW()
+					&& this.nemo2.getY() > this.nemo1.getY() - this.nemo1.getH()
+					&& this.nemo2.getY() < this.nemo1.getY() + this.nemo1.getH()) {
+				this.nemo2.setX(this.nemo2.getX() + 1);
+			}
+		} else if (this.dir == this.UP) {
+			if (this.nemo2.getY() + this.nemo2.getH() == this.nemo1.getY()
+					&& this.nemo2.getX() > this.nemo1.getX() - this.nemo1.getW()
+					&& this.nemo2.getX() < this.nemo1.getX() + this.nemo1.getW()) {
+				this.nemo2.setY(this.nemo2.getY() - 1);
+			}
 		}
 	}
 

75b2a9c9a01d4aa16a6d8ff0332e63954e5f7cbb
Administrator <Administrator@121.162.238.82>
2021-11-11 18:15:42 +0900

    몸통 통과 수정

--- gui/src/basic/Test_007_Snake.java
+++ gui/src/basic/Test_007_Snake.java
@@ -17,37 +17,37 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-class SnakeResultFrame extends JFrame{
+class SnakeResultFrame extends JFrame {
 	private Dimension dm = Toolkit.getDefaultToolkit().getScreenSize();
 	private final int W = this.dm.width;
 	private final int H = this.dm.height;
 	private final int SIZE = 500;
 	private JLabel title = new JLabel();
+
 	public SnakeResultFrame() {
 		setLayout(null);
-		setBounds(this.W / 2 - 250, this.H / 2 - 125,this.SIZE,this.SIZE - 250);
+		setBounds(this.W / 2 - 250, this.H / 2 - 125, this.SIZE, this.SIZE - 250);
 		setBackground(Color.white);
 		setTitle();
 		setVisible(true);
 		revalidate();
 	}
-	
+
 	private void setTitle() {
-		this.title.setBounds(0,0,this.SIZE,this.SIZE - 250);
+		this.title.setBounds(0, 0, this.SIZE, this.SIZE - 250);
 		this.title.setText("GAME OVER");
 		this.title.setHorizontalAlignment(JLabel.CENTER);
 		this.title.setVerticalAlignment(JLabel.CENTER);
 		this.title.setForeground(new Color(207, 183, 132));
-		this.title.setFont(new Font("NanumGothicBold",Font.BOLD, 50));
+		this.title.setFont(new Font("NanumGothicBold", Font.BOLD, 50));
 		add(this.title);
 	}
 }
 
-
 class SnakeRect {
 	private int x, y, w, h;
 	private Color c;
-	
+
 	public SnakeRect(int x, int y, int w, int h) {
 		this.x = x;
 		this.y = y;
@@ -95,93 +95,84 @@ class SnakeRect {
 	public void setC(Color c) {
 		this.c = c;
 	}
-	
+
 }
 
-class SnakePanel extends JPanel implements KeyListener, Runnable, ActionListener{
+class SnakePanel extends JPanel implements KeyListener, Runnable, ActionListener {
 	private Random rn = new Random();
-	
+
 	private final int UP = 0;
 	private final int LEFT = 1;
 	private final int DOWN = 2;
 	private final int RIGHT = 3;
-	
+
 	private final int SIZE = 800;
-	
+
 	private final int MAPSIZE = 10;
 	private final int FIRSTSNAKESIZE = 4;
 	private final int ITEMCNT = 10;
 	private final int WALL00 = 150;
-	
+
 	private JLabel title = new JLabel();
 	private JButton reset = new JButton();
 	private JButton start = new JButton();
-	
+
 	private SnakeRect[][] map = new SnakeRect[this.MAPSIZE][this.MAPSIZE];
 	private ArrayList<SnakeRect> snake;
 	private ArrayList<SnakeRect> item;
 
 	private Color snakeHC = new Color(110, 203, 99);
 	private Color snakeBC = new Color(177, 230, 147);
-	
-	
+
 	private int dir;
+	private int befodir;
 	private boolean gameEnd = true;
 	private boolean startAble = true;
-	
-	
-	private ArrayList <Integer[]> itemYX;
-	
+	private boolean first = true;
+
+	private ArrayList<Integer[]> itemYX;
+
 	public SnakePanel() {
 		setLayout(null);
-		setBounds(0,0,this.SIZE,this.SIZE);
+		setBounds(0, 0, this.SIZE, this.SIZE);
 		setBackground(Color.white);
-		
+
 		setTitle();
 		setStart();
 		setReset();
 		setMap();
 		setSnake();
 		setItem();
-		
+
 		setFocusable(true);
 		addKeyListener(this);
 	}
-	
-	
 
-	private void printItemList() {
-		for(int i = 0; i < this.itemYX.size(); i++) {
-			int y = this.itemYX.get(i)[0];
-			int x = this.itemYX.get(i)[0];
-			System.out.printf("y : %d /// x : %d\n",y,x);
-		}
-	}
-	
 	private void setTitle() {
-		this.title.setBounds(240,30,500,100);
+		this.title.setBounds(240, 30, 500, 100);
 		this.title.setText("SNAKE GAME");
 		this.title.setForeground(new Color(207, 183, 132));
 		this.title.setHorizontalAlignment(JLabel.LEFT);
-		this.title.setFont(new Font("NanumGothicBold",Font.BOLD, 50));
+		this.title.setFont(new Font("NanumGothicBold", Font.BOLD, 50));
 		add(this.title);
 	}
-	
+
 	private void setStart() {
 		this.start.setText("START");
-		this.start.setBounds(this.SIZE - 260 , this.SIZE - 100, 100, 60);
+		this.start.setBounds(this.SIZE - 260, this.SIZE - 100, 100, 60);
 		this.start.setForeground(Color.white);
 		this.start.setBackground(Color.black);
-		this.start.setFont(new Font("NanumGothicBold",Font.BOLD, 20));
+		this.start.setFont(new Font("NanumGothicBold", Font.BOLD, 20));
 		this.start.addActionListener(this);
 		add(this.start);
 	}
+
 	private void setReset() {
 		this.reset.setText("RESET");
-		this.reset.setBounds(this.SIZE - 150 , this.SIZE - 100, 100, 60);
+		this.reset.setBounds(this.SIZE - 150, this.SIZE - 100, 100, 60);
 		this.reset.setForeground(Color.white);
 		this.reset.setBackground(Color.black);
-		this.reset.setFont(new Font("NanumGothicBold",Font.BOLD, 20));
+		this.reset.setFont(new Font("NanumGothicBold", Font.BOLD, 20));
 		this.reset.addActionListener(this);
 		add(this.reset);
 	}
@@ -190,8 +181,8 @@ class SnakePanel extends JPanel implements KeyListener, Runnable, ActionListener
 		this.dir = this.LEFT;
 		int x = WALL00;
 		int y = WALL00;
-		for(int i = 0; i < this.MAPSIZE; i++) {
-			for(int j = 0; j < this.MAPSIZE; j++) {
+		for (int i = 0; i < this.MAPSIZE; i++) {
+			for (int j = 0; j < this.MAPSIZE; j++) {
 				this.map[i][j] = new SnakeRect(x, y, 50, 50);
 				x += 50;
 			}
@@ -202,83 +193,79 @@ class SnakePanel extends JPanel implements KeyListener, Runnable, ActionListener
 
 	private void setSnake() {
 		// 4칸
-		this.snake = new ArrayList <> ();
+		this.snake = new ArrayList<>();
 		int x = WALL00 + 50 + this.rn.nextInt(this.MAPSIZE - 4) * 50;
 		int y = WALL00 + this.rn.nextInt(this.MAPSIZE) * 50;
-		for(int i = 0; i < this.FIRSTSNAKESIZE; i ++) {
-			this.snake.add(new SnakeRect(x,y,50,50));
-			if(i == 0) {
+		for (int i = 0; i < this.FIRSTSNAKESIZE; i++) {
+			this.snake.add(new SnakeRect(x, y, 50, 50));
+			if (i == 0) {
 				this.snake.get(i).setC(this.snakeHC);
-			}
-			else {
+			} else {
 				this.snake.get(i).setC(this.snakeBC);
 			}
 			x += 50;
 		}
 	}
-	
+
 	private void setItem() {
 		// 10 개
-		this.item = new ArrayList <> (); 
-		this.itemYX = new ArrayList <>();
-		for(int i = 0; i < this.ITEMCNT; i ++) {
-			while(true) {
+		this.item = new ArrayList<>();
+		this.itemYX = new ArrayList<>();
+		for (int i = 0; i < this.ITEMCNT; i++) {
+			while (true) {
 				int x = WALL00 + this.rn.nextInt(this.MAPSIZE) * 50 + 13;
 				int y = WALL00 + this.rn.nextInt(this.MAPSIZE) * 50 + 13;
-				
+
 				// 스네이크랑 겹치지 않게
 				boolean check = false;
 				SnakeRect temp = this.snake.get(0);
-				
-				if(i == 0) {
-					if((x <= temp.getX() - temp.getW() || x >= temp.getX() + temp.getW() * this.snake.size()
+
+				if (i == 0) {
+					if ((x <= temp.getX() - temp.getW() || x >= temp.getX() + temp.getW() * this.snake.size()
 							|| y <= temp.getY() - temp.getH() || y >= temp.getY() + temp.getH() * this.snake.size())) {
 						this.item.add(new SnakeRect(x, y, 26, 26));
 						this.item.get(this.item.size() - 1).setC(new Color(255, 224, 93));
-						Integer [] tempInt = new Integer [2];
+						Integer[] tempInt = new Integer[2];
 						tempInt[0] = y - 13;
 						tempInt[1] = x - 13;
 						this.itemYX.add(tempInt);
 						check = true;
 					}
-				}
-				else {
+				} else {
 					boolean inChk = true;
-					for(int j = 0; j < this.item.size(); j++) {
+					for (int j = 0; j < this.item.size(); j++) {
 						SnakeRect temp2 = this.item.get(j);
-						if((x <= temp.getX() - temp.getW() || x >= temp.getX() + temp.getW() * this.snake.size()
+						if ((x <= temp.getX() - temp.getW() || x >= temp.getX() + temp.getW() * this.snake.size()
 								|| y <= temp.getY() - temp.getH() || y >= temp.getY() + temp.getH() * this.snake.size())
 								&& (x != temp2.getX() || y != temp2.getY())) {
-						}
-						else {
+						} else {
 							inChk = false;
 						}
 					}
-					
-					if(inChk) {
+
+					if (inChk) {
 						this.item.add(new SnakeRect(x, y, 26, 26));
 						this.item.get(this.item.size() - 1).setC(new Color(255, 224, 93));
-						Integer [] tempInt = new Integer [2];
+						Integer[] tempInt = new Integer[2];
 						tempInt[0] = y - 13;
 						tempInt[1] = x - 13;
 						this.itemYX.add(tempInt);
 						check = true;
 					}
-					
+
 				}
-				
-				if(check) {
+
+				if (check) {
 					break;
 				}
 			}
 		}
 	}
-	
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void gameReset() {
@@ -291,34 +278,48 @@ class SnakePanel extends JPanel implements KeyListener, Runnable, ActionListener
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() instanceof JButton) {
+		if (e.getSource() instanceof JButton) {
 			JButton target = (JButton) e.getSource();
-			if(target == this.start && this.startAble) {
+			if (target == this.start && this.startAble) {
 				this.gameEnd = false;
-			}
-			else if(target == this.reset) {
+			} else if (target == this.reset) {
 				gameReset();
 			}
 		}
 	}
-	
+
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if(e.getKeyCode() == e.VK_UP) {
+		SnakeRect temp = this.snake.get(0);
+		
+		if (e.getKeyCode() == e.VK_UP) {
 			System.out.println("UP");
+			this.befodir = this.dir;
 			this.dir = this.UP;
-		}
-		else if(e.getKeyCode() == e.VK_LEFT) {
+			if(backChkUp(temp)) {
+				this.dir = this.befodir;
+			}
+		} else if (e.getKeyCode() == e.VK_LEFT) {
 			System.out.println("LEFT");
+			this.befodir = this.dir;
 			this.dir = this.LEFT;
-		}
-		else if(e.getKeyCode() == e.VK_DOWN) {
+			if(backChkLeft(temp)) {
+				this.dir = this.befodir;
+			}
+		} else if (e.getKeyCode() == e.VK_DOWN) {
 			System.out.println("DOWN");
+			this.befodir = this.dir;
 			this.dir = this.DOWN;
-		}
-		else if(e.getKeyCode() == e.VK_RIGHT) {
+			if(backChkDown(temp)) {
+				this.dir = this.befodir;
+			}
+		} else if (e.getKeyCode() == e.VK_RIGHT) {
 			System.out.println("RIGHT");
+			this.befodir = this.dir;
 			this.dir = this.RIGHT;
+			if(backChkRight(temp)) {
+				this.dir = this.befodir;
+			}
 		}
 	}
 
@@ -326,42 +327,109 @@ class SnakePanel extends JPanel implements KeyListener, Runnable, ActionListener
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 	}
-	
+
 	private void drawMap(Graphics g) {
 		for (int i = 0; i < this.MAPSIZE; i++) {
 			for (int j = 0; j < this.MAPSIZE; j++) {
 				if (this.map[i][j] != null) {
 					g.setColor(new Color(250, 238, 224));
-					g.fillRect(this.map[i][j].getX(), this.map[i][j].getY(), this.map[i][j].getW(),this.map[i][j].getH());
+					g.fillRect(this.map[i][j].getX(), this.map[i][j].getY(), this.map[i][j].getW(),
+							this.map[i][j].getH());
 					g.setColor(Color.white);
-					g.drawRect(this.map[i][j].getX(), this.map[i][j].getY(), this.map[i][j].getW(),this.map[i][j].getH());
+					g.drawRect(this.map[i][j].getX(), this.map[i][j].getY(), this.map[i][j].getW(),
+							this.map[i][j].getH());
 				}
 			}
 		}
 	}
-	
+
 	private void drawSnake(Graphics g) {
-		for(int i = 0; i < this.snake.size(); i++) {
+		for (int i = 0; i < this.snake.size(); i++) {
 			SnakeRect temp = this.snake.get(i);
 			g.setColor(temp.getC());
-			g.fillRect(temp.getX(), temp.getY(), temp.getW(),temp.getH());
+			g.fillRect(temp.getX(), temp.getY(), temp.getW(), temp.getH());
 			g.setColor(Color.white);
-			g.drawRect(temp.getX(), temp.getY(), temp.getW(),temp.getH());
+			g.drawRect(temp.getX(), temp.getY(), temp.getW(), temp.getH());
 		}
 	}
-	
-	private void drawItem(Graphics g){
-		for(int i = 0; i < this.item.size(); i++) {
+
+	private void drawItem(Graphics g) {
+		for (int i = 0; i < this.item.size(); i++) {
 			SnakeRect temp = this.item.get(i);
-			if(temp != null) {
+			if (temp != null) {
 				g.setColor(temp.getC());
-				g.fillRoundRect(temp.getX(), temp.getY(), temp.getW(),temp.getH(), temp.getW(),temp.getH());
+				g.fillRoundRect(temp.getX(), temp.getY(), temp.getW(), temp.getH(), temp.getW(), temp.getH());
 				g.setColor(new Color(221, 190, 190));
-				g.drawRoundRect(temp.getX(), temp.getY(), temp.getW(),temp.getH(), temp.getW(),temp.getH());
+				g.drawRoundRect(temp.getX(), temp.getY(), temp.getW(), temp.getH(), temp.getW(), temp.getH());
 			}
 		}
 	}
-	
+
+	private boolean backChkUp(SnakeRect temp) {
+		boolean chk = false;
+		
+		int yy = temp.getY() - temp.getH();
+		int xx = temp.getX();
+		
+		for(int i = 1; i < this.snake.size(); i++) {
+			SnakeRect t = this.snake.get(i);
+			if(yy == t.getY() && xx == t.getX()) {
+				chk = true;
+				System.out.println("백은 안돼");
+			}
+		}
+		
+		return chk;
+	}
+	private boolean backChkLeft(SnakeRect temp) {
+		boolean chk = false;
+		
+		int yy = temp.getY();
+		int xx = temp.getX() - temp.getW();
+		
+		for(int i = 1; i < this.snake.size(); i++) {
+			SnakeRect t = this.snake.get(i);
+			if(yy == t.getY() && xx == t.getX()) {
+				chk = true;
+				System.out.println("백은 안돼");
+			}
+		}
+		
+		return chk;
+	}
+	private boolean backChkDown(SnakeRect temp) {
+		boolean chk = false;
+		
+		int yy = temp.getY() + temp.getH();
+		int xx = temp.getX();
+		
+		for(int i = 1; i < this.snake.size(); i++) {
+			SnakeRect t = this.snake.get(i);
+			if(yy == t.getY() && xx == t.getX()) {
+				chk = true;
+				System.out.println("백은 안돼");
+			}
+		}
+		
+		return chk;
+	}
+	private boolean backChkRight(SnakeRect temp) {
+		boolean chk = false;
+		
+		int yy = temp.getY();
+		int xx = temp.getX() + temp.getW();
+		
+		for(int i = 1; i < this.snake.size(); i++) {
+			SnakeRect t = this.snake.get(i);
+			if(yy == t.getY() && xx == t.getX()) {
+				chk = true;
+				System.out.println("백은 안돼");
+			}
+		}
+		
+		return chk;
+	}
+
 	private boolean wallChkUp(SnakeRect temp) {
 		boolean wallChk = false;
 		if (temp.getY() == this.WALL00) { // 이동하기 전 벽 좌표이면
@@ -393,33 +461,33 @@ class SnakePanel extends JPanel implements KeyListener, Runnable, ActionListener
 		}
 		return wallChk;
 	}
-	
+
 	private boolean itemChk(int mHeadY, int mHeadX) {
 		boolean itemChk = false;
-		for(int i = 0; i < this.itemYX.size(); i++) {
-			if(this.itemYX.get(i)[0] == mHeadY && this.itemYX.get(i)[1] == mHeadX) {
+		for (int i = 0; i < this.itemYX.size(); i++) {
+			if (this.itemYX.get(i)[0] == mHeadY && this.itemYX.get(i)[1] == mHeadX) {
 				itemChk = true;
 			}
 		}
 		return itemChk;
 	}
-	
+
 	private boolean bodyChk(int mHeadY, int mHeadX) {
 		boolean bodyChk = false;
-		if(this.snake.size() >= 5) {
-			for(int i = 3; i < this.snake.size() - 1; i++) {
-				if(this.snake.get(i).getX() == mHeadX && this.snake.get(i).getY() == mHeadY) {
+		if (this.snake.size() >= 5) {
+			for (int i = 3; i < this.snake.size() - 1; i++) {
+				if (this.snake.get(i).getX() == mHeadX && this.snake.get(i).getY() == mHeadY) {
 					bodyChk = true;
 				}
 			}
 		}
 		return bodyChk;
 	}
-	
+
 	private void wallM(ArrayList<SnakeRect> tempSnake) {
 		this.snake.removeAll(this.snake);
-		for(int i = 0; i < tempSnake.size() - 1; i++) {
-			if(i == 0) {
+		for (int i = 0; i < tempSnake.size() - 1; i++) {
+			if (i == 0) {
 				tempSnake.get(i).setC(snakeBC);
 			}
 			this.snake.add(tempSnake.get(i));
@@ -428,33 +496,33 @@ class SnakePanel extends JPanel implements KeyListener, Runnable, ActionListener
 		this.startAble = false;
 		new SnakeResultFrame();
 	}
-	
+
 	private void itemM(int mHeadY, int mHeadX, ArrayList<SnakeRect> tempSnake) {
 		int itemIdx = -1;
-		for(int i = 0; i < this.itemYX.size(); i++) {
-			if(this.itemYX.get(i)[0] == mHeadY && this.itemYX.get(i)[1] == mHeadX) {
+		for (int i = 0; i < this.itemYX.size(); i++) {
+			if (this.itemYX.get(i)[0] == mHeadY && this.itemYX.get(i)[1] == mHeadX) {
 				itemIdx = i;
 			}
 		}
 		this.item.remove(itemIdx);
 		this.itemYX.remove(itemIdx);
 		ArrayList<SnakeRect> temp = new ArrayList<>();
-		SnakeRect hS = new SnakeRect(mHeadX, mHeadY , 50, 50);
+		SnakeRect hS = new SnakeRect(mHeadX, mHeadY, 50, 50);
 		hS.setC(this.snakeHC);
 		temp.add(hS);
-		for(int i = 0; i < tempSnake.size(); i++) {
-			if(i == 0) {
+		for (int i = 0; i < tempSnake.size(); i++) {
+			if (i == 0) {
 				tempSnake.get(i).setC(this.snakeBC);
 			}
 			temp.add(tempSnake.get(i));
 		}
 		this.snake = temp;
 	}
-	
+
 	private void bodyM(ArrayList<SnakeRect> tempSnake) {
 		this.snake.removeAll(this.snake);
-		for(int i = 0; i < tempSnake.size() - 1; i++) {
-			if(i == 0) {
+		for (int i = 0; i < tempSnake.size() - 1; i++) {
+			if (i == 0) {
 				tempSnake.get(i).setC(snakeBC);
 			}
 			this.snake.add(tempSnake.get(i));
@@ -463,24 +531,23 @@ class SnakePanel extends JPanel implements KeyListener, Runnable, ActionListener
 		this.startAble = false;
 		new SnakeResultFrame();
 	}
-	
+
 	private void notingM(int mHeadY, int mHeadX, ArrayList<SnakeRect> tempSnake) {
 		this.snake.removeAll(this.snake);
-		SnakeRect head = new SnakeRect(mHeadX, mHeadY, 50, 50) ;
+		SnakeRect head = new SnakeRect(mHeadX, mHeadY, 50, 50);
 		head.setC(this.snakeHC);
 		this.snake.add(head);
-		for(int i = 0; i < tempSnake.size() - 1; i++) {
-			if(i == 0) {
+		for (int i = 0; i < tempSnake.size() - 1; i++) {
+			if (i == 0) {
 				tempSnake.get(i).setC(snakeBC);
 			}
 			this.snake.add(tempSnake.get(i));
 		}
 	}
-	
+
 	private void autoMoving() {
-		if(!this.gameEnd) {
+		if (!this.gameEnd) {
 			System.out.println("오토중");
-			printItemList();
 			System.out.printf("뱀 사이즈 : %d\n", this.snake.size());
 			SnakeRect temp = this.snake.get(0); // 머리만 확인 나머지는 앞의 좌표 물려받음
 			ArrayList<SnakeRect> tempSnake = new ArrayList<>(); // 이전 좌표 저장
@@ -490,7 +557,7 @@ class SnakePanel extends JPanel implements KeyListener, Runnable, ActionListener
 			if (this.dir == this.UP) {
 				int mHeadX = temp.getX();
 				int mHeadY = temp.getY() - temp.getH();
-				
+
 				boolean wallChk = wallChkUp(temp);
 				boolean itemChk = itemChk(mHeadY, mHeadX);
 				boolean bodyChk = bodyChk(mHeadY, mHeadX);
@@ -510,10 +577,13 @@ class SnakePanel extends JPanel implements KeyListener, Runnable, ActionListener
 				// 벽 또는 몸일 경우 머리, 꼬리 삭제 후 게임 종료
 				int mHeadX = temp.getX() - temp.getW();
 				int mHeadY = temp.getY();
+
+				
 				
 				boolean wallChk = wallChkLeft(temp);
 				boolean itemChk = itemChk(mHeadY, mHeadX);
 				boolean bodyChk = bodyChk(mHeadY, mHeadX);
+				
 				if (itemChk) {
 					itemM(mHeadY, mHeadX, tempSnake);
 				} else if (wallChk) {
@@ -526,11 +596,11 @@ class SnakePanel extends JPanel implements KeyListener, Runnable, ActionListener
 			} else if (this.dir == this.DOWN) {
 				int mHeadX = temp.getX();
 				int mHeadY = temp.getY() + temp.getH();
-				
+
 				boolean wallChk = wallChkDown(temp);
 				boolean itemChk = itemChk(mHeadY, mHeadX);
 				boolean bodyChk = bodyChk(mHeadY, mHeadX);
-				
+
 				if (wallChk) {
 					wallM(tempSnake);
 				} else if (itemChk) {
@@ -547,6 +617,7 @@ class SnakePanel extends JPanel implements KeyListener, Runnable, ActionListener
 				boolean wallChk = wallChkRight(temp);
 				boolean itemChk = itemChk(mHeadY, mHeadX);
 				boolean bodyChk = bodyChk(mHeadY, mHeadX);
+				
 				if (itemChk) {
 					itemM(mHeadY, mHeadX, tempSnake);
 				} else if (wallChk) {
@@ -579,31 +650,31 @@ class SnakePanel extends JPanel implements KeyListener, Runnable, ActionListener
 
 	@Override
 	public void run() {
-		while(true) {
-			if(this.snake.size() != 0) {
+		while (true) {
+			if (this.snake.size() != 0) {
 				autoMoving();
 			}
 		}
 	}
 }
 
-class Snake extends JFrame{
+class Snake extends JFrame {
 	private Dimension dm = Toolkit.getDefaultToolkit().getScreenSize();
 	private final int W = this.dm.width;
 	private final int H = this.dm.height;
 	private final int SIZE = 900;
 	private SnakePanel panel = new SnakePanel();
-	
+
 	public Snake() {
 		setLayout(null);
-		setBounds(this.W / 2 - 415, this.H / 2 - 415,this.SIZE,this.SIZE);
+		setBounds(this.W / 2 - 415, this.H / 2 - 415, this.SIZE, this.SIZE);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+
 		add(this.panel);
-		
+
 		setVisible(true);
 		revalidate();
-		
+
 		this.panel.run();
 	}
 }
@@ -611,7 +682,7 @@ class Snake extends JFrame{
 public class Test_007_Snake {
 
 	public static void main(String[] args) {
-		Snake  game = new Snake();
+		Snake game = new Snake();
 		// 초 당 자동으로 한칸 이동
 		// 자신의 몸에 부딪히면 사망
 		// 벽에 부딪히면 사망
--- gui/src/basic/Test_007_Snake_정답.java
+++ gui/src/basic/Test_007_Snake_정답.java
@@ -2,36 +2,155 @@ package basic;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-class SnakeRect01{
+class SnakeRect01 {
 	private int x, y, w, h;
 	private Color c;
+
 	public SnakeRect01(int x, int y, int w, int h, Color c) {
-		
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.c = c;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getW() {
+		return w;
+	}
+
+	public int getH() {
+		return h;
+	}
+
+	public Color getC() {
+		return c;
+	}
+
+	public void setC(Color c) {
+		this.c = c;
 	}
 }
 
+class Game001 extends JPanel implements KeyListener, ActionListener {
 
-class Game001 extends JPanel implements KeyListener{
+	private final int SIZE = 10;
+	private SnakeRect01[][] map = new SnakeRect01[this.SIZE][this.SIZE];
+
+	private SnakeRect01[] snake = new SnakeRect01[4]; // <- ArrayList<SnakeRect01>
+	private int[][] yx = new int[4][2]; // map 인데스 좌표 정보를 기억
+										// {{y,x},{y,x},{y,x},{y,x}};
+
+	private ArrayList<SnakeRect01> snake2 = new ArrayList<>();
+	private ArrayList<ArrayList<Integer>> yx2 = new ArrayList<>();
 	
-	private final int SIZE = 10; 
-	private SnakeRect01 [] [] map = new SnakeRect01[this.SIZE][this.SIZE];
+	private ArrayList<SnakeRect01> items = new ArrayList<>();
+	private ArrayList<ArrayList<Integer>> itemYx = new ArrayList<>();
 	
-	private SnakeRect01 [] snakeRect01 = new SnakeRect01[4]; // <- ArrayList<SnakeRect01>
-	private int [][] yx = new int [4][2];										   // map 인데스 좌표 정보를 기억
-																								   // {{y,x},{y,x},{y,x},{y,x}};
+	
+	private final int LEFT = 0;
+	private final int DOWN = 1;
+	private final int RIGHT = 2;
+	private final int UP = 3;
+	private final int STOP = 4;
+	
+	
+	private JButton [] btn = new JButton[4];
+	private JButton reset = new JButton();
+	
+	private int dir;
 	
 	public Game001() {
 		setLayout(null);
-		setBounds(0,0,700,500);
-		
+		setBounds(0, 0, 700, 500);
+
+		setMap();
+		setSnake();
+		setButton();
+
 		setFocusable(true); // KeyListener 사용할 때 필요
 		addKeyListener(this);
+	}
+
+	private void setButton() {
+		String [] text = {"←","↓","→","↑"};
+		
+		int x = 700 - 200;
+		int y = 500 - 200;
+		
+		for(int i = 0; i < this.btn.length; i++) {
+			this.btn[i] = new JButton();
+			this.btn[i].setBounds(x, y, 50, 50);
+			this.btn[i].setText(text[i]);
+			this.btn[i].addActionListener(this);
+			add(this.btn[i]);
+			
+			x += 50;
+			
+			if(i == this.btn.length -1 -1) {
+				x -= 100;
+				y -= 50;
+			}
+		}
+		
+		this.reset = new JButton();
+		this.reset.setBounds(700 - 200, y + 110, 150, 50);
+		this.reset.setText("RESET");
+		this.reset.addActionListener(this);
+		add(this.reset);
+	}
+
+	private void setSnake() {
+		for(int i = 0; i < this.snake.length; i ++) {
+			SnakeRect01 temp  = this.map[0][i];
+			int x = temp.getX();
+			int y = temp.getY();
+			int w = temp.getW();
+			int h = temp.getH();
+			Color c = Color.pink;
+			if(i == 0 ) {
+				c = Color.red;
+			}
+			this.snake[i] = new SnakeRect01(x, y, w, h, c);
+			
+			// 좌표기억
+			int [] pair = new int [2]; 
+			pair[0] = 0;	// map *배열*의 인덱스 y
+			pair[1] = i; 	// map *배열*의 인덱스 x
+			this.yx[i] = pair;
+		}
+	}
+
+	private void setMap() {
+		this.dir = this.STOP;
+		
+		int x = (700 - 200) / 2 - 40 * this.SIZE / 2;
+		int y = 500 / 2 - 40 * this.SIZE / 2;
+		for (int i = 0; i < this.SIZE; i++) {
+			for (int j = 0; j < this.SIZE; j++) {
+				this.map[i][j] = new SnakeRect01(x, y, 40, 40, Color.gray);
+				x += 40;
+			}
+			x = (700 - 200) / 2 - 40 * this.SIZE / 2;
+			y += 40;
+		}
 	}
 
 	@Override
@@ -42,44 +161,162 @@ class Game001 extends JPanel implements KeyListener{
 	@Override
 	public void keyPressed(KeyEvent e) {
 		System.out.println(e.getKeyCode());
-		
-		if(e.getKeyCode() == e.VK_ENTER) { // 키 고유값이 elements 상수로 저장되어 있음
+
+		if (e.getKeyCode() == e.VK_ENTER) { // 키 고유값이 elements 상수로 저장되어 있음
 			System.out.println("enter");
 		}
+
+		if (e.getKeyCode() == e.VK_LEFT) {
+			this.dir = this.LEFT;
+		}
+		else if (e.getKeyCode() == e.VK_DOWN) {
+			this.dir = this.DOWN;
+		}
+		else if (e.getKeyCode() == e.VK_RIGHT) {
+			this.dir = this.RIGHT;
+		}
+		else if (e.getKeyCode() == e.VK_UP) {
+			this.dir = this.UP;
+		}
 		
-		if(e.getKeyCode() == e.VK_LEFT) {}
-		if(e.getKeyCode() == e.VK_DOWN) {}
-		if(e.getKeyCode() == e.VK_RIGHT) {}
-		if(e.getKeyCode() == e.VK_UP) {}
-		
+		// update();
+		if (this.dir != this.STOP) {
+			update();
+		}
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		this.dir = this.STOP;
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		// TODO Auto-generated method stub
 		super.paintComponent(g);
+
+		// snake
+		for(int i = 0; i < this.snake.length; i++) {
+			SnakeRect01 r = this.snake[i];
+			g.setColor(r.getC());
+			g.fillRect(r.getX(), r.getY(), r.getW(), r.getH());
+		}
 		
+		// map
+		for (int i = 0; i < this.SIZE; i++) {
+			for (int j = 0; j < this.SIZE; j++) {
+				SnakeRect01 r = this.map[i][j];
+				g.setColor(r.getC());
+				g.drawRect(r.getX(), r.getY(), r.getW(), r.getH());
+			}
+		}
+
 		requestFocusInWindow(); // ← 키 리스너를 위한 포커스를 재 요청
 		repaint();
 	}
+
+	private void update() {
+		int yy = this.yx[0][0];
+		int xx = this.yx[0][1];
+		
+		// moving
+		if(this.dir == this.LEFT) {
+			xx --;
+		}
+		else if(this.dir == this.DOWN) {
+			yy ++;
+		}
+		else if(this.dir == this.RIGHT) {
+			xx ++;
+		}
+		else if(this.dir == this.UP) {
+			yy --;
+		}
+		
+		if(xx < 0 || xx >= this.SIZE || yy < 0 || yy >= this.SIZE) {
+			return;
+		}
+		
+		boolean bodyCheck = false;
+		for(int i = 0; i < this.yx.length; i++) {
+			if(yy == this.yx[i][0] && xx == this.yx[i][1]) {
+				bodyCheck = true;
+			}
+		}
+		
+		if(bodyCheck) {
+			return;
+		}
+		
+		// move
+		
+		// body
+		for(int i = this.snake.length - 1; i > 0; i--) {
+			this.snake[i] = this.snake[i - 1];
+			this.snake[i].setC(Color.pink);
+			
+			// 좌표 갱신
+//			this.yx[i] = this.yx[i - 1]; // <<  주소 공유 해서 오류
+			int [] pair = new int [2];
+			pair[0] = this.yx[i-1][0];
+			pair[1] = this.yx[i-1][1];
+			this.yx[i] = pair;
+		}
+		
+		// head
+		SnakeRect01 t = this.map[yy][xx];
+		this.snake[0] = new SnakeRect01(t.getX(), t.getY(), t.getW(), t.getH(), Color.red);
+		// 좌표 갱신
+		this.yx[0][0] = yy; // <
+		this.yx[0][1] = xx;
+	}
 	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() instanceof JButton) {
+			JButton target = (JButton) e.getSource();
+			
+			if(target == this.reset) {
+				resetMap();
+			}
+			else {
+				if(target == this.btn[LEFT]) {
+					this.dir = LEFT;
+				}
+				else if(target == this.btn[DOWN]) {
+					this.dir = DOWN;
+				}
+				else if(target == this.btn[RIGHT]) {
+					this.dir = RIGHT;
+				}
+				else if(target == this.btn[UP]) {
+					this.dir = UP;
+				}
+				// update();
+				if(this.dir != this.STOP) {
+					update();
+				}
+			}
+		}
+	}
+
+	private void resetMap() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
 
-class Snake01 extends JFrame{
+class Snake01 extends JFrame {
 	public Snake01() {
 		super("snake");
 		setLayout(null);
 		setBounds(100, 100, 700, 500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+
 		add(new Game001());
-		
+
 		setVisible(true);
 		revalidate();
 	}

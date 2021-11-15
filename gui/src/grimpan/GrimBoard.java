197bd184bd4fdece7f26717ba07eabf14852608c
Administrator <Administrator@121.162.238.82>
2021-11-15 19:17:53 +0900

    삼각형 첫 좌표 기준

--- gui/src/grimpan/GrimBoard.java
+++ gui/src/grimpan/GrimBoard.java
@@ -2,18 +2,34 @@ package grimpan;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
 
 public class GrimBoard extends MyUtil{
+	private ArrayList<GrimRect> rects = new ArrayList<>();
+	private ArrayList<GrimRect> circles = new ArrayList<>();
 	
 	private GrimRect rect = null;
 	private int startX, startY;
 	private boolean shift;
 	
+	private final int RECTANGLE = 0;
+	private final int CIRCLE = 1;
+	private final int TRIANGLE = 2;
+	
+	private String [] btnText = {"□","○","△"};
+	private JButton []  btn = new JButton[3];
+	private int type;
+	
 	public GrimBoard() {
 		setLayout(null);
 		setBounds(0,0,700,700);
+		
+		setButton();
 		
 		addMouseListener(this);
 		addMouseMotionListener(this);
@@ -23,13 +39,69 @@ public class GrimBoard extends MyUtil{
 		
 	}
 	
+	private void setButton() {
+		int x = 30;
+		int y = 50;
+		for(int i = 0; i < this.btn.length; i++) {
+			this.btn[i] = new JButton();
+			this.btn[i].setBounds(x, y, 50, 50);
+			this.btn[i].setText(this.btnText[i]);
+			this.btn[i].addActionListener(this);
+			
+			add(this.btn[i]);
+			
+			y += 50 + 3;
+		}
+	}
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
+		// sample triangle
+		// g.drawPolygon(int[], int[], int)
+		// 1. x 좌표의 배열
+		// 2. y 좌표의 배열
+		// 3. 좌표 개수 (꼭지점 개수)
+		int [] xx = new int[3];
+		int [] yy = new int[3];
+		xx[0] = 100;
+		yy[0] = 100;
+		xx[1] = 150;
+		yy[1] = 200;
+		xx[2] = 50;
+		yy[2] = 200;
+		g.setColor(Color.GREEN);
+		g.drawPolygon(xx, yy, 3);
+		
+		// this.rect : 임시 객체 - > 타입에 따라 다르게
 		if(this.rect != null) {
 			g.setColor(this.rect.getC());
-			g.drawRect(this.rect.getX(), this.rect.getY(), this.rect.getW(), this.rect.getH());
+
+			if(this.type == this.RECTANGLE) {
+				g.drawRect(this.rect.getX(), this.rect.getY(), this.rect.getW(), this.rect.getH());
+			}
+			else if (this.type == this.CIRCLE) {
+				g.drawRoundRect(this.rect.getX(), this.rect.getY(), this.rect.getW(), this.rect.getH(), this.rect.getW(), this.rect.getH());
+			}
+			else if (this.type == this.TRIANGLE) {
+				
+			}
+		}
+		
+		
+		// rects
+		for(int i =0; i < this.rects.size(); i++) {
+			GrimRect r = this.rects.get(i);
+			g.setColor(r.getC());
+			g.drawRect(r.getX(), r.getY(), r.getW(), r.getH());
+		}
+		// circles
+		
+		for(int i =0; i < this.circles.size(); i++) {
+			GrimRect r = this.circles.get(i);
+			g.setColor(r.getC());
+			g.drawRoundRect(r.getX(), r.getY(), r.getW(), r.getH(), r.getW(), r.getH());
 		}
 		
 		requestFocusInWindow();
@@ -59,6 +131,16 @@ public class GrimBoard extends MyUtil{
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		this.rect.setC(Color.blue);
+		
+		if(this.type == this.RECTANGLE) {
+			this.rects.add(this.rect);
+		}
+		else if (this.type == this.CIRCLE) {
+			this.circles.add(this.rect);
+		}
+		else if (this.type == this.TRIANGLE) {
+		}
+		this.rect = null;
 	}
 	
 	@Override
@@ -85,5 +167,18 @@ public class GrimBoard extends MyUtil{
 			rY =this.startY - h;
 		}
 		this.rect = new GrimRect(rX, rY, w, h, Color.red);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == this.btn[this.RECTANGLE]) {
+			this.type = this.RECTANGLE;
+		}
+		else if(e.getSource() == this.btn[this.CIRCLE]) {
+			this.type = this.CIRCLE;
+		}
+		else if(e.getSource() == this.btn[this.TRIANGLE]) {
+			this.type = this.TRIANGLE;
+		}
 	}
 }
--- gui2/src/drawingBoard/GrimBoard.java
+++ gui2/src/drawingBoard/GrimBoard.java
@@ -3,6 +3,7 @@ package drawingBoard;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -11,6 +12,7 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 
 public class GrimBoard extends MyUtil{
@@ -19,29 +21,54 @@ public class GrimBoard extends MyUtil{
 	
 	private GrimRect rect = null;
 	
+	private final int NEMO = 0;
+	private final int CIRCLE = 1;
+	private final int SEMO = 2;
+	
+	private int chkForm;
 	private int firstX;
 	private int firstY;
+	private int [] triX = new int [3]; 
+	private int [] triY = new int [3]; 
 	
 	private boolean shift = false;
 	
-	private ArrayList<GrimRect> done = new ArrayList<>();
+	private ArrayList<GrimRect> rects = new ArrayList<>();
+	private ArrayList<GrimRect> circles = new ArrayList<>();
+	private ArrayList<int []> triangles = new ArrayList<>();
 	
 	private JLabel help = new JLabel("'ESC' Press = RESET");
+	private JLabel showShift = new JLabel("'Shift' = false");
+	
+	String [] btnText = {"□","○","△"};
+	private JButton []  btn = new JButton[3];
 	
 	public GrimBoard() {
 		setLayout(null);
 		setBounds(0,0,1000,1000);
 		setBackground(Color.white);
 		
+		this.chkForm = 0;
+		
 		setHelp();
+		setShowShift();
+		setBtn();
 		
 		addMouseListener(this);
 		addMouseMotionListener(this);
 		
-		addKeyListener(this);
 		setFocusable(true); // KeyListener 사용시 필요 외우기
+		addKeyListener(this);
 	}
 	
+	private void setShowShift() {
+		this.showShift.setBounds(800,0,155,30);
+		this.showShift.setForeground(Color.red);
+		this.showShift.setHorizontalAlignment(JLabel.RIGHT);
+		this.showShift.setFont(new Font("NanumGothicBold",Font.PLAIN, 17));
+		add(this.showShift);
+	}
+
 	public void setHelp() {
 		this.help.setBounds(10,0,155,30);
 		this.help.setForeground(Color.red);
@@ -49,23 +76,83 @@ public class GrimBoard extends MyUtil{
 		add(this.help);
 	}
 	
+	private void setBtn() {
+		int x = 830;
+		int y = 950;
+		
+		for(int i = 0; i < this.btn.length; i++) {
+			this.btn[i] = new JButton();
+			this.btn[i].setBounds(x,y, 50, 30);
+			this.btn[i].setText(this.btnText[i]);
+			this.btn[i].addActionListener(this);
+			add(this.btn[i]);
+			x += 53;
+		}
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() instanceof JButton) {
+			JButton target = (JButton) e.getSource();
+			if(target == this.btn[this.NEMO]) {
+				this.chkForm = this.NEMO;
+			}
+			else if (target == this.btn[this.CIRCLE]) {
+				this.chkForm = this.CIRCLE;
+			}
+			else if (target == this.btn[this.SEMO]) {
+				this.chkForm = this.SEMO;
+			}
+		}
+	}
+	
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
 		if(this.rect != null) {
 			g.setColor(this.rect.getC());
-			g.drawRect(this.rect.getX(), this.rect.getY(), this.rect.getW(), this.rect.getH());
+			if(this.chkForm == this.NEMO) {
+				g.drawRect(this.rect.getX(), this.rect.getY(), this.rect.getW(), this.rect.getH());
+			}
+			else if(this.chkForm == this.CIRCLE) {
+				g.drawRoundRect(this.rect.getX(), this.rect.getY(), this.rect.getW(), this.rect.getH(), this.rect.getW(), this.rect.getH());
+			}
+			else if(this.chkForm == this.SEMO) {
+				g.drawPolygon(this.triX, this.triY, 3);
+			}
 		}
 		
-		for(int i = 0; i < this.done.size(); i++) {
-			if(this.done.get(i) != null) {
-				GrimRect a = this.done.get(i);
+		// rects
+		for(int i = 0; i < this.rects.size(); i++) {
+			if(this.rects.get(i) != null) {
+				GrimRect a = this.rects.get(i);
 				g.setColor(a.getC());
 				g.drawRect(a.getX(), a.getY(), a.getW(), a.getH());
 			}
 		}
 		
+		
+		// circles
+		
+		for(int i = 0; i < this.circles.size(); i++) {
+			if(this.circles.get(i) != null) {
+				GrimRect a = this.circles.get(i);
+				g.setColor(a.getC());
+				g.drawRoundRect(a.getX(), a.getY(), a.getW(), a.getH(), a.getW(), a.getH());
+			}
+		}
+		
+		// triangle
+		for(int i = 0; i < this.triangles.size(); i += 2) {
+			if(this.triangles.get(i) != null) {
+				int x [] = this.triangles.get(i);
+				int y [] = this.triangles.get(i + 1);
+				g.setColor(Color.blue);
+				g.drawPolygon(x, y, 3);
+			}
+		}
+		
+		requestFocusInWindow(); // 키리스너 필수
 		repaint(); // 다시 그리기, 오버라이딩 하자마자 추가
 	}
 	
@@ -73,13 +160,15 @@ public class GrimBoard extends MyUtil{
 	public void mousePressed(MouseEvent e) { // 첫 좌표 받기
 		this.firstX = e.getX();
 		this.firstY = e.getY();
+		this.triX[0] = this.firstX;
+		this.triY[0] = this.firstY;
 	}
 	
 	@Override
 	public void mouseDragged(MouseEvent e) {
+		// 현재 마우스 좌표
 		int x = e.getX();
 		int y = e.getY();
-		
 		// 프레임 or 패널 위에서의 좌표는 무조건 0과 양수 값
 		// 기준값 - 이동값 또는 이동값 - 기준값 해도
 		// 절대값 처리하면 거리값으로 구해질 수 있음
@@ -87,18 +176,12 @@ public class GrimBoard extends MyUtil{
 		// 절대값 메소드 Math.abs();
 		int w = Math.abs(x - this.firstX);
 		int h = Math.abs(y - this.firstY);
-		
 		if(this.shift) {
-			if(w > h) {
-				h = w;
-			}
-			else if(h > w) {
-				w = h;
-			}
-			else {
-				h = w;
-			}
+			h = w;
 		}
+		
+		this.triX[1] = this.firstX - (w / 2);
+		this.triX[2] = this.firstX + (w / 2);
 		
 		int paintingX = this.firstX;
 		int paintingY = this.firstY;
@@ -108,7 +191,14 @@ public class GrimBoard extends MyUtil{
 		}
 		if(y < this.firstY) {
 			paintingY = this.firstY - h;
+			this.triY[1] = this.firstY - h;
+			this.triY[2] = this.firstY - h;
 		}
+		else {
+			this.triY[1] = this.firstY + h;
+			this.triY[2] = this.firstY + h;
+		}
+		
 		
 		this.rect = new GrimRect(paintingX, paintingY, w, h, Color.black);
 	}
@@ -117,15 +207,34 @@ public class GrimBoard extends MyUtil{
 	public void mouseReleased(MouseEvent e) {
 		if(this.rect != null) {
 			GrimRect temp = new GrimRect(this.rect.getX(), this.rect.getY(), this.rect.getW(), this.rect.getH(), Color.blue);
-			this.done.add(temp);
+			if(this.chkForm == this.NEMO) {
+				this.rects.add(temp);
+			}
+			else if (this.chkForm == this.CIRCLE) {
+				this.circles.add(temp);
+			}
+			else if (this.chkForm == this.SEMO) {
+				int [] tempX = new int [3];
+				int [] tempY = new int [3];
+				
+				for(int i = 0 ; i < this.triX.length; i++) {
+					tempX[i] = this.triX[i];
+					tempY[i] = this.triY[i];
+				}
+				this.triangles.add(tempX);
+				this.triangles.add(tempY);
+			}
 			this.rect = null;
 		}
+		
 	}
 	
 	@Override
 	public void keyPressed(KeyEvent e) {
 		if(e.getKeyCode() == e.VK_SHIFT) {
 			this.shift = true;
+			this.showShift.setForeground(Color.blue);
+			this.showShift.setText("'Shift' = true");
 		}
 	}
 	
@@ -133,9 +242,13 @@ public class GrimBoard extends MyUtil{
 	public void keyReleased(KeyEvent e) {
 		if(e.getKeyCode() == e.VK_SHIFT) {
 			this.shift = false;
+			this.showShift.setForeground(Color.red);
+			this.showShift.setText("'Shift' = flase");
 		}
 		if(e.getKeyCode() == e.VK_ESCAPE) {
-			this.done = new ArrayList<>();
+			this.rects = new ArrayList<>();
+			this.circles = new ArrayList<>();
+			this.triangles = new ArrayList<>();
 		}
 	}
 }
